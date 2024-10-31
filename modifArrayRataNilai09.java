559dd8c34be784d4f7db28a7bdba4184d9fe640f
Fata-Haidar <fatahaidaraly@gmail.com>
2024-10-31 10:59:58 +0700

    modifPertanyaan3

---
+++ modifArrayRataNilai09.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class modifArrayRataNilai09 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiMhs = new int[10];
+        double total=0, totalp=0;
+        int lulus = 0, tdkLulus=0;
+        double rata2lulus, rata2Tdklulus;
+        System.out.print("Masukkan jumlah mahasiswa : "  );
+        int jmlhMH = sc.nextInt();
+        for(int i=0; i < jmlhMH; i++){
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : " );
+            nilaiMhs[i] = sc.nextInt();
+        } 
+        int x = 0;
+        for(int i = 0; i < jmlhMH; i++){
+             
+            if (nilaiMhs[i] > 70) {
+                lulus+=1;
+                total+=nilaiMhs[i];
+                
+
+                
+            }   else{
+                totalp+=nilaiMhs[i];
+                tdkLulus+=1;
+            }
+             
+        } 
+        rata2lulus = total/lulus;
+        rata2Tdklulus = totalp/tdkLulus;
+        System.out.println("Rata-rata nilai lulus= "+rata2lulus);
+        System.out.println("Rata-rata nilai tidak lulus= "+rata2Tdklulus);
+        
+    }
+}
