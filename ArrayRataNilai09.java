388df7aae3d3f7893c8ea1a61a1ff721553121b0
Fata-Haidar <fatahaidaraly@gmail.com>
2024-10-31 07:53:00 +0700

    Percobaan3

--- ArrayRataNilai09.java
+++ ArrayRataNilai09.java
@@ -8,7 +8,7 @@ public class ArrayRataNilai09 {
         Scanner sc = new Scanner(System.in);
         int[] nilaiMhs = new int[10];
         double total=0;
-        double rata2;y
+        double rata2;
 
         for(int i=0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : " );
